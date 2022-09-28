ab8b5aeaf3664c9d46522e280edb869180f61a17
Paulo Augusto Teixeira <paulo.teixeira@wooza.com.br>
2022-09-28 11:20:13 -0300

    criacao das tabelas do projeto

---
+++ src/database/migrations/2022_09_28_140601_currency_locations.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CurrencyLocations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currency_locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('currency_id')->unsigned();
+            $table->foreign('currency_id')->references('id')->on('currencies');
+            $table->string('location',255);
+            $table->string('icon',255)->nullable();
+         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currcurrency_locationsencies');
+    }
+}

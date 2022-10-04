37ce08b40d5c0bbf055c185187c0f266d484cdc6
Paulo Teixeira <paulo.teixeira@alliedbrasil.com.br>
2022-10-04 09:48:44 -0300

    done

--- src/app/Services/CrawlerService.php
+++ src/app/Services/CrawlerService.php
@@ -11,13 +11,15 @@ class CrawlerService
     public $client;
     public $url;
 
-    public function __construct( String $url ) {
+    public function __construct( String $url ) 
+    {
         $this->client = new Client(HttpClient::create(['timeout' => self::SIXTY_SECONDS_DURATION]));
         $this->client->setServerParameter('HTTP_USER_AGENT', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0');
         $this->url = $url;
     }
 
-    function crawlingPage() {
+    function crawlingPage() 
+    {
         $crawler = $this->client->request('GET', $this->url);
         
         return $crawler;
